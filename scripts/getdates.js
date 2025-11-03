7ce1056be5ce25299b2f529cb7bfa1870731e1a4
emilyurena94 <emilyurena974@gmail.com>
2025-11-03 18:03:33 -0500

    Add files via upload

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,1 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const currentYear = new Date().getFullYear();
+    document.getElementById("currentyear").textContent = currentYear;
 
+    const lastModified = document.lastModified;
+    document.getElementById("lastmodified").textContent = lastModified;
+});
